a0a2e79c58418b3067968c6fd6a2fcca7d99e680
zbm2024 <165012694+zbm2024@users.noreply.github.com>
2026-08-12 06:39:27 +0800

    Add files via upload

---
+++ TE-Speed-MiniMaxH3/__init__.py
@@ -0,0 +1,12 @@
+from .nodes import TESpeedMiniMaxH3
+
+
+NODE_CLASS_MAPPINGS = {
+    "TESpeedMiniMaxH3": TESpeedMiniMaxH3,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "TESpeedMiniMaxH3": "TE-Speed-MiniMaxH3",
+}
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
